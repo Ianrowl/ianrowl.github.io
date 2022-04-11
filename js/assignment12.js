a6326305f91d850a10aef5cbbc4956020cde59b9
Ianrowl <iaro8747@colorado.edu>
2022-04-11 00:25:45 -0600

    h

--- js/assignment12.js
+++ js/assignment12.js
@@ -3,22 +3,29 @@ async function getFact(){
   console.log("Facts button was clicked")
 
   try{
-    const response = await fetch(end);
+    const response = await fetch(end)
     if(!response.ok){
       throw Error(response.statusText)
     }
 
     const json = await response.json();
     console.log(json);
-  }
+    displayFact(json.fact);
 
+  }
   catch(err){
-    console.log(err)
+    console.log(err);
     alert('Something failed');
   }
 }
 
-const end = ''
+function displayFact(fact){
+  const factText = document.querySelector('#js-fact-text');
+  factText.textContent = fact;
+}
 
-const factsButton document.querySelector('#js-new-quote');
+// http://numbersapi.com/random/math
+const end = 'http://numbersapi.com/random/math';
+
+const factsButton = document.querySelector('#js-new-fact');
 factsButton.addEventListener('click', getFact);
