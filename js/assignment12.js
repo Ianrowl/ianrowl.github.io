394f0c1e3faaa8be64d94cb3c667f6f33aaa295e
Ianrowl <iaro8747@colorado.edu>
2022-04-11 18:10:35 -0600

    Kanye

--- js/assignment12.js
+++ js/assignment12.js
@@ -7,10 +7,18 @@ async function getFact(){
     if(!response.ok){
       throw Error(response.statusText)
     }
+    const response2 = await fetch(end2)
+    if(!response2.ok){
+      throw Error(response2.statusText)
+    }
 
     const json = await response.json();
     console.log(json);
     displayFact(json.text);
+
+    const json2 = await response2.json();
+    console.log(json2);
+    displayFact2(json2.quote);
 
   }
   catch(err){
@@ -24,8 +32,15 @@ function displayFact(fact){
   factText.textContent = fact;
 }
 
+function displayFact2(fact2){
+  const factText2 = document.querySelector('#js-fact-2');
+  factText2.textContent = fact2;
+}
+
 // http://numbersapi.com/random/math
 const end = 'https://uselessfacts.jsph.pl/random.json';
+
+const end2 = 'https://api.kanye.rest';
 
 const factsButton = document.querySelector('#js-new-fact');
 factsButton.addEventListener('click', getFact);
