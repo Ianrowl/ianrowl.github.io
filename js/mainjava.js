dbaea45e56d4efd51e423435b56bd65b3c8fff6f
Ianrowl <iaro8747@colorado.edu>
2022-05-01 18:03:47 -0600

    y

--- js/mainjava.js
+++ js/mainjava.js
@@ -40,46 +40,5 @@ function setGreeting(){
   }
 }
 
-// displayTime();
+displayTime();
 setGreeting();
-
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-
-const prevButton = document.querySelector('#prev');
-const nextButton = document.querySelector('#next');
-
-//Counter to make sure which photo we're on
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-
-nextButton.addEventListener('click',()=>{
-  if(counter >= carouselImages.length-1) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-});
-
-prevButton.addEventListener('click',()=>{
-  if(counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', ()=>{
-  if(carouselImages[counter].id === 'cloneLast'){
-    carouselSlide.style.transition = "none";
-    console.log('none');
-    counter = carouselImages.length - 2;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-  }
-  if(carouselImages[counter].id === 'cloneFirst'){
-    carouselSlide.style.transition = "none";
-    console.log('none');
-    counter = carouselImages.length - counter;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-  }
-});
