07ab3eadc3838b3e1b5ad9c619011220aa465dae
Ianrowl <iaro8747@colorado.edu>
2022-05-13 22:42:20 -0600

    Good Night.

--- js/mainjava.js
+++ js/mainjava.js
@@ -5,17 +5,20 @@ function setGreeting(){
   let today = new Date(),
     hour = today.getHours();
 
-  if(hour < 6){
+  if(hour < 6 && hour > 1){
     document.getElementById("greeting").textContent = "Hello, you're up early.";
   }
   else if(hour < 12) {//It's morning
     document.getElementById("greeting").textContent = 'Good Morning.';
   }
-  else if(hour < 18){//It's the Afternoon
+  else if(hour < 17){//It's the Afternoon
     document.getElementById("greeting").textContent = 'Good Afternoon.';
   }
-  else{//It's the evening
+  else if(hour < 21){//It's the evening
     document.getElementById("greeting").textContent = 'Good Evening.';
+  }
+  else{
+    document.getElementById("greeting").textContent = 'Good Night.';
   }
 }
 
